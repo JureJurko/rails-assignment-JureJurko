cf644c0d612c4067288f48df725d0c86b16d3b67
JureJurko <jurefranjkovic2002@gmail.com>
2022-07-22 12:22:49 +0200

    Fix OpenWeatherMap

--- app/domains/open_weather_map.rb
+++ app/domains/open_weather_map.rb
@@ -2,12 +2,16 @@ module OpenWeatherMap
   APPID = Rails.application.credentials.open_weather_map_api_key
   def self.city(name)
     id = Resolver.city_id(name)
+    return nil unless id
+
     hash = HTTP.get("https://api.openweathermap.org/data/2.5/weather?id=#{id}&appid=#{APPID}")
     City.parse(hash)
   end
 
   def self.cities(names)
     ids = names.map { |name| Resolver.city_id(name) }.compact
+    return nil if ids.blank?
+
     hash = HTTP.get("https://api.openweathermap.org/data/2.5/group?id=#{ids.join(',')}&appid=#{APPID}")
     hash['list'].map { |hsh| City.parse(hsh) }
   end
