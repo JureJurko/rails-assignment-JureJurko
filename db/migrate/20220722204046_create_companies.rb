b3bda6aaae9a55219d6060fd62dc2888ac12b053
JureJurko <jurefranjkovic2002@gmail.com>
2022-07-24 14:28:05 +0200

    Fix more problems

--- db/migrate/20220722204046_create_companies.rb
+++ db/migrate/20220722204046_create_companies.rb
@@ -1,7 +1,7 @@
 class CreateCompanies < ActiveRecord::Migration[6.1]
   def change
     create_table :companies do |t|
-      t.string :name, null: false, unique: { case_sensitive: false }
+      t.string :name, null: false, index: { unique: { case_sensitive: false } }
 
       t.timestamps
     end
