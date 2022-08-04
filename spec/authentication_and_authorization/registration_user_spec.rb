10ea8b84a4ced8478c4f7439d9397d25f962a5ea
JureJurko <jurefranjkovic2002@gmail.com>
2022-08-04 02:49:25 +0200

    Add registration specs

---
+++ spec/authentication_and_authorization/registration_user_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe User do
+  let(:user) { FactoryBot.create(:user, token: 'abc') }
+
+  it 'updates when given correct password' do
+    user.password = 'abcd'
+    expect(user.save).to eq(true)
+  end
+
+  it 'returns error when given wrong password' do
+    user.password = nil
+    user.save
+    expect(user.errors['password']).to include("can't be blank")
+    user.password = ''
+    user.save
+    expect(user.errors['password']).to include("can't be blank")
+  end
+end
